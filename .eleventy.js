19f509bffdadcb474036d9ccdaf30f653fa64230
James Hall <james@agile.coop>
2022-11-04 08:19:14 +0000

    Initial commit.

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+module.exports = function (eleventyConfig) {
+  // Copy the `img` and `css` folders to the output
+  eleventyConfig.addWatchTarget('./src/sass/')
+  eleventyConfig.addPassthroughCopy('./src/css')
+  eleventyConfig.addPassthroughCopy('./src/images')
+
+  // Override Browsersync defaults (used only with --serve)
+  eleventyConfig.setBrowserSyncConfig({
+    ui: false,
+    port: 8181,
+    host: '0.0.0.0',
+    ghostMode: false,
+  })
+
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: 'njk',
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: 'njk',
+
+    // These are all optional (defaults are shown):
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: 'public',
+    },
+  }
+}
