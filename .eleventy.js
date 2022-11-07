99d46eddd46cdc7635282769813d793dc692eb4e
James Hall <james@agile.coop>
2022-11-07 12:26:47 +0000

    Add responsive images.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
+const path = require('path')
+
+const Image = require('@11ty/eleventy-img')
 
 module.exports = function (eleventyConfig) {
+  require('dotenv').config()
+  eleventyConfig.addGlobalData('env', process.env)
+
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addWatchTarget('./src/sass/')
   eleventyConfig.addWatchTarget('./src/js/')
@@ -15,6 +21,47 @@ module.exports = function (eleventyConfig) {
     host: '0.0.0.0',
     ghostMode: false,
   })
+
+  // --- START, eleventy-img
+  function imageShortcode(
+    src,
+    alt,
+    priority = false,
+    sizes = '(min-width: 1024px) 100vw, 50vw'
+  ) {
+    src = `src${src}`
+    console.log(`Generating image(s) from:  ${src}`)
+    let options = {
+      widths: [600, 900, 1500],
+      formats: ['webp', 'jpeg'],
+      urlPath: '/images/',
+      outputDir: './public/images/',
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src)
+        const name = path.basename(src, extension)
+        return `${name}-${width}w.${format}`
+      },
+    }
+
+    // generate images
+    Image(src, options)
+
+    let imageAttributes = {
+      alt,
+      sizes,
+    }
+    if (!priority) {
+      imageAttributes = {
+        ...imageAttributes,
+        ...{ loading: 'lazy', decoding: 'async' },
+      }
+    }
+    // get metadata
+    metadata = Image.statsSync(src, options)
+    return Image.generateHTML(metadata, imageAttributes)
+  }
+  eleventyConfig.addShortcode('image', imageShortcode)
+  // --- END, eleventy-img
 
   return {
     // Control which files Eleventy will process
